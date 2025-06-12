3def4b8c79e2a16ae9e3513f9dd5c9d2ec8b7ca2
ikariLain <matheus-torrico@outlook.com>
2025-06-12 10:55:42 +0200

    Update global CSS and variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import Logo from '../public/Flower.svg'
-import './App.css'
-
+import React from 'react';
+import Header from './Header/Header';
+import Footer from './Footer/Footer';
+import Sections from './Sections/Sections';
+import './App.css';
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={Logo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header />
+      <Sections />
+      <Footer />
     </>
   )
 }
