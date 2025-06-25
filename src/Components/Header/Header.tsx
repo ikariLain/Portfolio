4062631e9901e02a38da5aa9f93f9e00e0a13621
ikariLain <matheus-torrico@outlook.com>
2025-06-25 10:59:31 +0200

    Fixed Header glassy style and siderbar logic and style

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,24 +1,76 @@
+import { useState } from 'react';
+import Bars from '../../assets/BarsWhite.png';
+import Xmark from '../../assets/XmarkWhite.png';
+
 import './Header.css'
 
 export default function Header  (){
+
+    const [MenuOpen, SetMenuOpen] = useState(false);
+
+    const ToggleMenu = () => {
+        SetMenuOpen(!MenuOpen);
+    }
+
+    const CloseMenu = () => {
+        SetMenuOpen(false);
+    }
+
+    const MenuItems = [
+        {id : 1, Name: 'About me', href: '#About'},
+        {id : 2, Name: 'Skills', href: '#Skills'},
+        {id : 3, Name: 'Experiences', href: '#EXP'},
+        {id : 4, Name: 'Projects', href: '#Projects'},
+        {id : 5, Name: 'Contact', href: '#Contact'}
+    ]
+
+
     return (
     <>
       <header className='Header-Container'>
 
-          <div className='Navbar'>
-              <a href="#About;" className='Item'>About me</a>
 
-              <a href="#Skills"className='Item'>Skills</a>
-              <a href="#EXP" className='Item'>Experiences</a>
-              <a href="#Projects" className='Item'>Projects</a>
-              <a href="#Contact" className='Item'>Contact</a>
-          </div>
+          <nav className='Navbar'>
+                {MenuItems.map(item => (
+                    <a key={item.id} href={item.href} className='Item'>{item.Name}</a>
+                ))}
+            </nav>
+
 
           <div className='Dark-Or-Light-Mode-Container'>
               <div className='Light-Logo'></div>
               <div className='Dark-Logo'></div>
               <div className='Toggle-Symbol'></div>
           </div>
+
+        <div className={`Hamburger-Menu ${MenuOpen ? 'hidden' : ''}`} onClick={ToggleMenu}>
+                <img className='BarsImg' src={Bars} />
+            </div>
+
+
+
+           {/* Sidebar för mobil */}
+            <div className={`Sidebar ${MenuOpen ? 'Open' : ''}`}>
+                <div className="Sidebar-Header">
+                    <img
+                        src={Xmark}
+                        className="Close-Icon"
+                        onClick={CloseMenu}
+                    />
+                </div>
+                <div className="Sidebar-Items">
+                    {MenuItems.map(item => (
+                        <a
+                            key={item.id}
+                            href={item.href}
+                            className='Item'
+                            onClick={CloseMenu}
+                        >
+                            {item.Name}
+                        </a>
+                    ))}
+                </div>
+            </div>
 
       </header>
     </>
