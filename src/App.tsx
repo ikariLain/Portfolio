b4a9389a1fb2fc927655c6c991b9198205f84f6a
ikariLain <matheus-torrico@outlook.com>
2025-11-30 23:20:12 +0100

    Fix footer

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,15 @@ import './index.css';
 function App() {
 
   return (
-    <>
+    <div className='min-h-screen flex flex-col'>
       <Header />
-      <main>
+      <main className='flex-1'>
       <Hero />
 
       </main>
 
       <Footer />
-    </>
+    </div>
   )
 }
 
--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,8 +1,18 @@
-import './Footer.css'
+//import './Footer.css'
 export default function Footer () {
   return (
-      <div className='Footer-Container'>
-        <p>Footer</p>
+      <div className='
+      w-full mt-20 py-6
+      flex flex-col items-center justify-center
+      bg-black/20 backdrop-blur-xl
+      border-t border-white/10
+      text-white/70
+      '>
+        <p className="
+        text-sm tracking-wide
+        ">
+          © {new Date().getFullYear()} Matheus Torrico. All rights reserved.
+          </p>
       </div>
   )
 }
